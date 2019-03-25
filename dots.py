a122c6f9139e68e2dbb6bd08861784f494628d8a
chuiizeet <chuy.guerra19@outlook.es>
2019-03-24 18:56:23 -0600

    TSP: Nearest Neighbor

--- dots.py
+++ dots.py
@@ -2,40 +2,41 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-# RGB colors
-mag = [(125,0,255),(255,0,255)]
-red =[(200,0,0),(255,100,100)]
-yell =[(175,255,0),(255,255,0)]
+def get_dots():
+    # RGB colors
+    mag = [(125,0,255),(255,0,255)]
+    red =[(200,0,0),(255,100,100)]
+    yell =[(175,255,0),(255,255,0)]
 
-maps = ['fountains','memories','shrines']
-colors = [mag,red,yell]
+    maps = ['fountains','memories','shrines']
+    colors = [mag,red,yell]
 
-for i,m in enumerate(maps):
+    for i,m in enumerate(maps):
 
-    # Load images
-    img = cv2.imread('maps/'+str(m)+'.png')
+        # Load images
+        img = cv2.imread('maps/'+str(m)+'.png')
 
-    # Blur images
-    blur_img = cv2.medianBlur(img,19)
+        # Blur images
+        blur_img = cv2.medianBlur(img,19)
 
-    # Smoothie
-    test = blur_img.copy()
-    test = test.astype(np.float32)/255
-    test = np.power(test,6)
+        # Smoothie
+        test = blur_img.copy()
+        test = test.astype(np.float32)/255
+        test = np.power(test,6)
 
-    # Thresholding
-    ret,th1 = cv2.threshold(test,0.45,1.0,cv2.THRESH_BINARY)
+        # Thresholding
+        ret,th1 = cv2.threshold(test,0.45,1.0,cv2.THRESH_BINARY)
 
-    # Uint8
-    x = th1.copy()
-    x = x.astype(np.uint8)*255
+        # Uint8
+        x = th1.copy()
+        x = x.astype(np.uint8)*255
 
-    # mask
-    mask1 = cv2.inRange(x,colors[i][0],colors[i][1])
+        # mask
+        mask1 = cv2.inRange(x,colors[i][0],colors[i][1])
 
-    # final mask and masked
-    mask = cv2.bitwise_or(mask1,mask1)
-    target = cv2.bitwise_and(x,x, mask=mask)
+        # final mask and masked
+        mask = cv2.bitwise_or(mask1,mask1)
+        target = cv2.bitwise_and(x,x, mask=mask)
 
-    # save images
-    cv2.imwrite('outputs/'+str(m)+'_dots.png',target)
+        # save images
+        cv2.imwrite('outputs/'+str(m)+'_dots.png',target)
---
+++ nearest_neighbor.py
@@ -0,0 +1,135 @@
+import cv2
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import time
+
+best_distance = 0.0
+best_tour = []
+
+
+def display(img,d):
+    fig = plt.figure(figsize=(8,6))
+    ax = fig.add_subplot(111)
+    plt.title('Distance = '+str(d))
+    ax.imshow(img)
+
+def draw_tsp(map,d,tsp,points):
+    copy_map = map.copy()
+    for line in range (0,len(tsp)-1):
+        last = line+1
+        # print(points[tsp[line]],points[tsp[last]])
+        if last == len(tsp)-1:
+            last = 0
+            cv2.line(copy_map,
+                     pt1=points[tsp[line]],
+                     pt2=points[tsp[last]],
+                     color=(255,0,0),
+                     thickness=4)
+        else:
+            cv2.line(copy_map,
+                     pt1=points[tsp[line]],
+                     pt2=points[tsp[last]],
+                     color=(255,0,0),
+                     thickness=4)
+
+    display(copy_map,d)
+
+def nearest_neightbor(inputMap):
+
+    maps_dots = ['fountains_dots','memories_dots','shrines_dots']
+    maps = ['fountains.png','memories.png','shrines.png']
+
+    img = cv2.imread('outputs/'+str(maps_dots[int(inputMap)])+'.png',0)
+    dots = np.zeros(img.shape,dtype=np.uint8)
+    map = cv2.imread('maps/'+str(maps[int(inputMap)]))
+    map_rgb = cv2.cvtColor(map,cv2.COLOR_BGR2RGB)
+
+    #####################################
+    ########    GET POINTS    ###########
+    #####################################
+
+    image,contours,hierarchy = cv2.findContours(img,
+                     cv2.RETR_CCOMP,
+                     cv2.CHAIN_APPROX_SIMPLE)
+    # Centroids
+    mnts  = [cv2.moments(cnt) for cnt in contours]
+    centroids = [( int(round(m['m10']/m['m00'])),int(round(m['m01']/m['m00'])) ) for m in mnts]
+
+    points = []
+    dots = np.zeros(img.shape)
+    for c in centroids:
+        cv2.circle(img=map_rgb,center=c,radius=8,color=(0,255,0),thickness=-1)
+        points.append(c)
+
+    f = 0
+    while f < len(points):
+
+        x = []
+        y = []
+
+        for i in points:
+            x.append(i[0])
+            y.append(i[1])
+
+        eu = []
+        tsp = []
+
+        for j in range (0,len(x)):
+            last = j+1
+            if last == len(x)-1:
+                last = 0
+                s = (pow(x[f]-x[j],2) + pow (y[f]-y[j],2)) #N
+                eu.append(math.sqrt(s))
+
+            else:
+                s = (pow(x[f]-x[j],2) + pow (y[f]-y[j],2)) #N
+                eu.append(math.sqrt(s))
+
+        # Sort indices
+        _indexSort = np.array(np.argsort(eu))
+
+        # Tour TSP
+        indexSort = list(_indexSort)
+
+        for x in range (0,len(indexSort)):
+            tsp.append(int(indexSort[x]))
+
+        # Insert
+        tsp.insert(len(tsp),f)
+
+
+        # Last distance
+        _lTour = points[tsp[len(tsp)-1]]
+        _lTour2 = points[tsp[len(tsp)-2]]
+        s = (pow(_lTour[0]-_lTour2[0],2) + pow (_lTour[1]-_lTour2[1],2))
+        eu.append(math.sqrt(s))
+
+        # Distance
+        distance = sum(eu)
+
+        # Draw lines
+        draw_tsp(map_rgb,distance,tsp,points)
+
+        # Best distance & tour
+        global best_distance, best_tour
+
+        if f == 0:
+            best_distance = distance
+            best_tour = tsp
+
+        if(distance < best_distance):
+            best_distance = distance
+            best_tour = tsp
+
+        # Feels
+        f += 1
+
+    print('\n')
+    print('Distance: ',best_distance)
+    print('Best tour: ',best_tour)
+    print('\n')
+    print('Tour coordinates:')
+    print('--------------------')
+    for r in best_tour:
+        print(points[r])
